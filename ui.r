cab46578068b306000f837b46b06b6509fdcf580
greengolddog <53335244+greengolddog@users.noreply.github.com>
2020-11-09 20:41:37 +0300

    Update ui.r

--- ui.r
+++ ui.r
@@ -4,38 +4,38 @@ fluidPage(
   radioButtons("Languages", label = h3("Language"),
                choices = list("English" = 1, "Russian" = 2),
                selected = 2),
-
+  
   tabsetPanel(
     type = "tabs",
     tabPanel(
       "User",
       sidebarPanel(
-      selectInput("programminglanguage", label = h3("Select Programming Language"), 
-                  choices = list("Pascal" = 1, "python" = 2, "C++" = 3), 
-                  selected = 1),
+        selectInput("programminglanguage", label = h3("Select Programming Language"), 
+                    choices = list("Pascal" = 1, "python" = 2, "C++" = 3), 
+                    selected = 1),
       ),
       mainPanel(
-      tabsetPanel(
-        type = "tabs",
-        tabPanel(
-          "Task A",
-          fileInput("fileu", label = h3("Put Your Code Here")),
-          actionButton("actionu", label = "Check"),
-          hr()
+        tabsetPanel(
+          type = "tabs",
+          tabPanel(
+            "Task A",
+            fileInput("fileA", label = h3("Put Your Code Here")),
+            actionButton("actionUA", label = "Check"),
+            hr()
+          ),
+          tabPanel(
+            "Task B",
+            fileInput("fileB", label = h3("Put Your Code Here")),
+            actionButton("actionUB", label = "Check"),
+            hr()
+          ),
+          tabPanel(
+            "Task C",
+            fileInput("fileC", label = h3("Put Your Code Here")),
+            actionButton("actionUC", label = "Check"),
+            hr()
+          )
         ),
-        tabPanel(
-          "Task B",
-          fileInput("fileu", label = h3("Put Your Code Here")),
-          actionButton("actionu", label = "Check"),
-          hr()
-        ),
-        tabPanel(
-          "Task C",
-          fileInput("fileu", label = h3("Put Your Code Here")),
-          actionButton("actionu", label = "Check"),
-          hr()
-        )
-      ),
       ),
     ),
     tabPanel(
@@ -45,22 +45,22 @@ fluidPage(
         tabPanel(
           "Task A",
           mainPanel(
-          actionButton("actionAA", label = "Add description"),
-          hr()
+            actionButton("actionAA", label = "Add description"),
+            hr()
           )
         ),
         tabPanel(
           "Task B",
           mainPanel(
-          actionButton("actionAB", label = "Add description"),
-          hr()
+            actionButton("actionAB", label = "Add description"),
+            hr()
           )
         ),
         tabPanel(
           "Task C",
           mainPanel(
-          actionButton("actionAC", label = "Add description"),
-          hr()
+            actionButton("actionAC", label = "Add description"),
+            hr()
           )
         )
       )
