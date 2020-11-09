75dc87965ba66cca9bb6efb90b982112ef83eff1
Hwrrys <53346468+hwrrys@users.noreply.github.com>
2020-11-09 20:35:25 +0300

    Update ui.r

--- ui.r
+++ ui.r
@@ -1,85 +1,67 @@
 library(shiny)
 fluidPage(
+  titlePanel("YetAnoterProject"),
+  radioButtons("Languages", label = h3("Language"),
+               choices = list("English" = 1, "Russian" = 2),
+               selected = 2),
+
   tabsetPanel(
     type = "tabs",
     tabPanel(
       "User",
-        tabsetPanel(
+      sidebarPanel(
+      selectInput("programminglanguage", label = h3("Select Programming Language"), 
+                  choices = list("Pascal" = 1, "python" = 2, "C++" = 3), 
+                  selected = 1),
+      ),
+      mainPanel(
+      tabsetPanel(
         type = "tabs",
-          tabPanel(
+        tabPanel(
           "Task A",
           fileInput("fileu", label = h3("Put Your Code Here")),
-          radioButtons("Languagesu", label = h3("Language"),
-                       choices = list("English" = 1, "Russian" = 2),
-                       selected = 2),
-          selectInput("programminglanguagea", label = h3("Select "), 
-                      choices = list("Task A" = 1, "Task B" = 2, "Task C" = 3), 
-                      selected = 1),
-          selectInput("programminglanguageu", label = h3("Select programming language"), 
-                      choices = list("Pascal.ABC" = 1, "Python 3" = 2, "C++" = 3), 
-                      selected = 1),
           actionButton("actionu", label = "Check"),
           hr()
         ),
         tabPanel(
           "Task B",
           fileInput("fileu", label = h3("Put Your Code Here")),
-          radioButtons("Languagesu", label = h3("Language"),
-                       choices = list("English" = 1, "Russian" = 2),
-                       selected = 2),
-          selectInput("programminglanguagea", label = h3("Select "), 
-                      choices = list("Task A" = 1, "Task B" = 2, "Task C" = 3), 
-                      selected = 1),
-          selectInput("programminglanguageu", label = h3("Select programming language"), 
-                      choices = list("Pascal.ABC" = 1, "Python 3" = 2, "C++" = 3), 
-                      selected = 1),
           actionButton("actionu", label = "Check"),
           hr()
         ),
         tabPanel(
           "Task C",
           fileInput("fileu", label = h3("Put Your Code Here")),
-          radioButtons("Languagesu", label = h3("Language"),
-                       choices = list("English" = 1, "Russian" = 2),
-                       selected = 2),
-          selectInput("programminglanguagea", label = h3("Select "), 
-                      choices = list("Task A" = 1, "Task B" = 2, "Task C" = 3), 
-                      selected = 1),
-          selectInput("programminglanguageu", label = h3("Select programming language"), 
-                      choices = list("Pascal.ABC" = 1, "Python 3" = 2, "C++" = 3), 
-                      selected = 1),
           actionButton("actionu", label = "Check"),
           hr()
         )
+      ),
       ),
     ),
     tabPanel(
       "Admin",
       tabsetPanel(
-      type = "tabs",
+        type = "tabs",
         tabPanel(
-        "Task A",
-        radioButtons("Languagesa", label = h3("Language"),
-                     choices = list("English" = 1, "Russian" = 2),
-                     selected = 2),
-        actionButton("actiona", label = "Add description"),
-        hr()
+          "Task A",
+          mainPanel(
+          actionButton("actionAA", label = "Add description"),
+          hr()
+          )
         ),
         tabPanel(
           "Task B",
-          radioButtons("Languagesa", label = h3("Language"),
-                       choices = list("English" = 1, "Russian" = 2),
-                       selected = 2),
-          actionButton("actiona", label = "Add description"),
+          mainPanel(
+          actionButton("actionAB", label = "Add description"),
           hr()
+          )
         ),
         tabPanel(
           "Task C",
-          radioButtons("Languagesa", label = h3("Language"),
-                       choices = list("English" = 1, "Russian" = 2),
-                       selected = 2),
-          actionButton("actiona", label = "Add description"),
+          mainPanel(
+          actionButton("actionAC", label = "Add description"),
           hr()
+          )
         )
       )
     )
