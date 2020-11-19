7f992261372cac88d75f313f267d542d56a40b1b
greengolddog <53335244+greengolddog@users.noreply.github.com>
2020-11-19 11:30:11 +0300

    Update ui.r

--- ui.r
+++ ui.r
@@ -1,9 +1,12 @@
-  library(shiny)
-  fluidPage(
-    titlePanel("YetAnoterProject"),
-    radioButtons("Languages", label = h3("Language"),
-                 choices = list("English" = 1, "Russian" = 2),
-                 selected = 2),
-    textInput("password","Password"),
-    uiOutput("ui") 
-  )
+library(shiny)
+fluidPage(
+  titlePanel("YetAnoterProject"),
+  radioButtons("Languages", label = h3("Language"),
+               choices = list("English" = 1, "Russian" = 2),
+               selected = 2),
+  h4("if you need to register enter Login: 'reg'"),
+  textInput("login","Login:"),
+  textInput("password","Password:"),
+  actionButton("Login", label = "Login"),
+  uiOutput("ui") 
+)
