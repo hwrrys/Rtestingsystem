3b2bf7d720c832406fe01b5e65bfe98ebb238826
greengolddog <loseff.pit@ya.ru>
2020-11-20 08:52:57 +0300

    I fix bats

--- c++.bat
+++ c++.bat
@@ -1,4 +1,4 @@
-set PATH=C:\Users\losef\Desktop\Rproject\mingw64\bin;%PATH%
+set PATH=mingw64\bin;%PATH%
 rem echo %PATH%
-rem cd "C:\Users\losef\Desktop\Rproject\mingw64\bin"\
+rem cd "mingw64\bin"\
 a.exe < in.txt > out.txt 2> ex.txt
--- co++.bat
+++ co++.bat
@@ -1,4 +1,4 @@
-set PATH=C:\Users\losef\Desktop\Rproject\mingw64\bin;%PATH%
+set PATH=mingw64\bin;%PATH%
 rem echo %PATH%
-rem cd "C:\Users\losef\Desktop\Rproject\mingw64\bin"
+rem cd "mingw64\bin"\
 g++ 1c.cpp 2> ex.txt
